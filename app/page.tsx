5b1bf18b17a9302d6c50d6cee472281db894cb9f
toniorocks <marco.izag@gmail.com>
2026-02-28 21:31:22 -0600

    feat: Configure page to force dynamic rendering and disable revalidation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from "next/link";
 import { getAllSpaceCrafts } from "@/lib/services/spacecraft.service";
 import { OpenModalTestButton } from "./components/modal/OpenModalTestButton";
